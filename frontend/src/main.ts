f420abaf770deed02d5909fedc428760088e513f
Christian Wendlinger <christian.wendlinger99@gmail.com>
2022-12-05 23:46:01 +0100

    basic project setup basic nav

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,9 +2,12 @@ import { bootstrapApplication } from "@angular/platform-browser";
 import { AppComponent } from "./app/app.component";
 import { provideRouter } from "@angular/router";
 import { APP_ROUTES } from "./app/app-routes";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { importProvidersFrom } from '@angular/core';
 
 bootstrapApplication(AppComponent, {
   providers: [
-    provideRouter(APP_ROUTES)
-  ]
+    provideRouter(APP_ROUTES),
+    importProvidersFrom(BrowserAnimationsModule)
+]
 });
