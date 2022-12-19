90970fbc94b6273862727cfb3181edbfd025ab4a
ardipelaj <ardi98@live.de>
2022-12-19 14:36:57 +0100

    login view

--- frontend/src/app/admin/users/users.component.ts
+++ frontend/src/app/admin/users/users.component.ts
@@ -3,11 +3,14 @@ import { CommonModule } from '@angular/common';
 import { MatIconModule } from "@angular/material/icon";
 import { RouterLink } from "@angular/router";
 import { MatButtonModule } from '@angular/material/button';
+import {MatInputModule} from "@angular/material/input";
+import {MatPaginatorModule} from "@angular/material/paginator";
+import {MatCheckboxModule} from "@angular/material/checkbox";
 
 @Component({
   selector: 'app-users',
   standalone: true,
-  imports: [CommonModule, MatIconModule, RouterLink, MatButtonModule],
+  imports: [CommonModule, MatIconModule, RouterLink, MatButtonModule, MatInputModule, MatPaginatorModule, MatCheckboxModule],
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.scss']
 })
--- frontend/src/app/shared/login/login.component.ts
+++ frontend/src/app/shared/login/login.component.ts
@@ -5,11 +5,12 @@ import {RouterLink} from "@angular/router";
 import {MatButtonModule} from "@angular/material/button";
 import {MatFormFieldModule} from "@angular/material/form-field";
 import {MatInputModule} from "@angular/material/input";
+import {MatCheckboxModule} from "@angular/material/checkbox";
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, MatIconModule, RouterLink, MatButtonModule, MatFormFieldModule, MatInputModule],
+  imports: [CommonModule, MatIconModule, RouterLink, MatButtonModule, MatFormFieldModule, MatInputModule, MatCheckboxModule],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
