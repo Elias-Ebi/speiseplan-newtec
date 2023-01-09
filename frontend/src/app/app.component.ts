4b55db257ecb7df560115ca7f83317752817ab6e
SireneQualle <57450915+SireneQualle@users.noreply.github.com>
2023-01-10 00:45:55 +0100

    Feature bestellungsverwaltung (#19)
    * basic ui for managing orders
    
    * modal dialogs, mior layout changes
    
    * added date pickers
    
    * manage orders
    
    * removed test classes
    restyled order management
    linked order management in admin routes
    
    Co-authored-by: Sarah Schaupp <sarah.schaupp@immersight.de>
    Co-authored-by: Christian Wendlinger <christian.wendlinger99@gmail.com>

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -3,6 +3,7 @@ import { UserNavigationComponent } from "./user/shared/components/navigation/use
 import { RouterOutlet } from "@angular/router";
 import { AuthService } from "./shared/services/auth.service";
 
+
 @Component({
   standalone: true,
   selector: 'app-root',
@@ -11,7 +12,8 @@ import { AuthService } from "./shared/services/auth.service";
     UserNavigationComponent,
     RouterOutlet
   ],
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  
 })
 export class AppComponent implements OnInit {
   constructor(private authService: AuthService) {
