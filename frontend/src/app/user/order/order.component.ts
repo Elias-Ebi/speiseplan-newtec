a4485eeed775b6bbecec802028f04abff82be138
Luca Uihlein <luca.uihlein@uni-ulm.de>
2023-01-10 03:49:11 +0100

    - Meine Bestellungen FE - Gastbestellungen FE

--- frontend/src/app/user/order/order.component.ts
+++ frontend/src/app/user/order/order.component.ts
@@ -1,13 +1,43 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { OrderCardComponent } from "../shared/components/order-card/order-card.component";
+import { MatIconModule } from "@angular/material/icon";
+import { MatButtonModule } from "@angular/material/button";
 
 @Component({
   selector: 'app-order',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, OrderCardComponent, MatIconModule, MatButtonModule],
   templateUrl: './order.component.html',
   styleUrls: ['./order.component.scss']
 })
 export class OrderComponent {
+  menues = [
+    {
+      icon: "assets/food_icons/meat_icon.png",
+      name: "Fleischküchle",
+      amount: 17,
+      description: "Fleischküchle in Zwiebelsoße mit Kartoffelpüree und Rahmkarotten",
+      ordered: false
+    },
+    {
+      icon: "assets/food_icons/vegan_icon.png",
+      name: "Bratlinge",
+      amount: 12,
+      description: "Grünkern-Linsen-Gemüse-Bratlinge mit Tomaten-Paprika Salsa und Grillgemüse",
+      ordered: true
+    },
+    {
+      icon: "assets/food_icons/salad_icon.png",
+      name: "Salat Sizilia",
+      amount: 3,
+      description: "Salat “Sizilia” Tomaten, Basilikum, Zuchinistreifen, Artischoken, Thunfisch, Oliven, Orangen und Balsamicodressing",
+      ordered: false
+    }
+  ];
 
+  guestOrders = [
+    {name: 'Ich', meal: 'Schnitzel'},
+    {name: 'Draco Malfoy', meal: 'Salat Sizilia'}
+  ];
 }
