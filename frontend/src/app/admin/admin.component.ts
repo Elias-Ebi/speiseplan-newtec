b16267c1bbb16b2bcf1d77304ee6149f30ccc557
Christian Wendlinger <christian.wendlinger99@gmail.com>
2023-02-02 22:08:05 +0100

    added admin navigation based on user navigation Gerichte and Nutzer are not linked yet

--- frontend/src/app/admin/admin.component.ts
+++ frontend/src/app/admin/admin.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from "@angular/router";
+import { AdminNavigationComponent } from "./shared/components/navigation/admin-navigation.component";
 
 @Component({
   selector: 'app-admin',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, AdminNavigationComponent],
   templateUrl: './admin.component.html',
   styleUrls: ['./admin.component.scss']
 })
