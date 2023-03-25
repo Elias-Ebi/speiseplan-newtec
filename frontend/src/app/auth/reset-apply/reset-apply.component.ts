ae6a3d477fe911c6d52103b1498426cc1858ee84
ardipelaj <ardi98@live.de>
2023-03-25 13:06:54 +0100

    added set new password function for reset password

--- frontend/src/app/auth/reset-apply/reset-apply.component.ts
+++ frontend/src/app/auth/reset-apply/reset-apply.component.ts
@@ -1,35 +1,88 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { MatIconModule } from '@angular/material/icon';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { AuthService } from 'src/app/shared/services/auth.service';
 
 @Component({
   selector: 'app-reset-apply',
   standalone: true,
-  imports: [CommonModule, MatButtonModule, MatFormFieldModule, ReactiveFormsModule, MatIconModule],
+  imports: [
+    CommonModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    MatIconModule,
+    RouterLink
+  ],
   templateUrl: './reset-apply.component.html',
-  styleUrls: ['./reset-apply.component.scss']
+  styleUrls: ['./reset-apply.component.scss'],
 })
-export class ResetApplyComponent {
+export class ResetApplyComponent implements OnInit {
   form: FormGroup = this.initializeForm();
-  hidePw = true;
+  hidePw: boolean = true;
+  PASSWORD_LENGTH: number = 8;
+  token: string = "";
 
-  constructor(private fb: FormBuilder, private authService: AuthService) {
+  constructor(
+    private fb: FormBuilder,
+    private router: Router,
+    private authService: AuthService,
+    private activeRoute: ActivatedRoute
+  ) {}
+
+  async ngOnInit() {
+    await this.activeRoute.params.subscribe((param) => {
+      if (typeof param['token'] === 'string') {
+        this.token = param['token'];
+      }
+    });
   }
 
   initializeForm(): FormGroup {
     return this.fb.group({
       password: [''],
-      password_confirm: ['']
-    })
+      password_confirm: [''],
+    });
   }
 
-  onSubmit() {
+  async setNewPassword() {
     if (this.form.valid) {
       const { password, password_confirm } = this.form.value;
+      if (password === password_confirm) {
+        await this.authService
+          .setNewPasswordFromResetToken(this.token, password)
+          .then(() => {
+            this.router.navigateByUrl('/auth/login');
+          });
+      }
+    }
+  }
+
+  shortenPassword(password: string, len: number): string {
+    let resultPassword = '';
+    if (password.length > len) {
+      for (let i = 0; i < len; i++) {
+        if (i < password.length) {
+          resultPassword += password.charAt(i);
+        }
+      }
+      return resultPassword;
+    } else {
+      return password;
+    }
+  }
+
+  shortenPasswordRec(password: string, len: number): string {
+    if (len <= 0) {
+      throw new Error('Length must be greater than 0');
+    } else if (len >= password.length) {
+      return password;
+    } else {
+      return this.shortenPasswordRec(password.slice(0, -1), len);
     }
   }
 
